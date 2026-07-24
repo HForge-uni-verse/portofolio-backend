391874b534e96fab6454f1ec33e28f10da19c668
HForge-uni-verse <sarruhassan@gmail.com>
2026-07-24 20:24:35 +0530

    Update email.js
    l

--- backend/config/email.js
+++ backend/config/email.js
@@ -7,12 +7,14 @@ const transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASS,
     },
 });
-transporter.verify((error, success) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log("SMTP connection successful");
-    }
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
 });
 
 module.exports = transporter;

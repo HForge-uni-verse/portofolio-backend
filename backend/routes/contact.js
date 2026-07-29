f9a9be3a0ccadf40811885d51c2394912a31249a
HForge-uni-verse <sarruhassan@gmail.com>
2026-07-29 11:47:13 +0530

    Update contact.js
    gg

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { Resend } = require("resend");
 
 const Contact = require("../models/contact");
-const transporter = require("../config/email");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 router.post("/", async (req, res) => {
-    try {
-        const { name, email, message } = req.body;
+  try {
+    const { name, email, message } = req.body;
 
-        const newContact = new Contact({
-            name,
-            email,
-            message
-        });
+    const newContact = new Contact({
+      name,
+      email,
+      message,
+    });
 
-        await newContact.save();
-        await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: process.env.EMAIL_USER,
-    subject: "📩 New Portfolio Contact",
-    text: `
+    await newContact.save();
+
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "sarruhassan@gmail.com", // Replace with your Gmail
+      subject: "📩 New Portfolio Contact",
+      text: `
 New Contact Form Submission
 
 Name: ${name}
@@ -27,23 +30,21 @@ Email: ${email}
 
 Message:
 ${message}
-`
-});
+      `,
+    });
 
-        res.status(201).json({
-            success: true,
-            message: "Message saved successfully"
-        });
-
-    } catch (error) {
+    res.status(201).json({
+      success: true,
+      message: "Message sent successfully",
+    });
+  } catch (error) {
     console.error("CONTACT ERROR:", error);
 
     res.status(500).json({
-        success: false,
-        message: error.message,
-        stack: error.stack
+      success: false,
+      message: error.message,
     });
-}
+  }
 });
 
 module.exports = router;

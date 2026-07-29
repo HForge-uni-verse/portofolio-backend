c746868599a6c562cd0bd9bac4c232935979cbc6
H <sarruhassan@gmail.com>
2026-07-29 17:47:30 +0530

    Add admin API routes

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { contact } = require("../controllers/contactController");
+const {
+    contact,
+    getContacts,
+    deleteContact,
+} = require("../controllers/contactController");
 
+// Create Contact
 router.post("/", contact);
+
+// Get All Contacts
+router.get("/", getContacts);
+
+// Delete Contact
+router.delete("/:id", deleteContact);
 
 module.exports = router;

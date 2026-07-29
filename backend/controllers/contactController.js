fe2ef5ea3221d8c2054f5014c7787b72bd4abaa1
H <sarruhassan@gmail.com>
2026-07-29 16:34:57 +0530

    Fix model import case

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,4 +1,4 @@
-const Contact = require("../models/Contact");
+const Contact = require("../models/contact");
 const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
